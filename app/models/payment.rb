860caf31c93bc43cb338e1e3d6b80f47099c1f22
AmanWorku <amexworku2014@gmail.com>
2023-07-02 21:03:19 +0300

    Add header for payments

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -5,7 +5,4 @@ class Payment <ApplicationRecord
     validates :name, presence: true, length: { maximum: 25 }
     validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
 
-    def total_transaction_amount
-        payments.sum(:amount)
-    end
 end
