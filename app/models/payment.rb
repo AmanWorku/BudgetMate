b159abf8473bb7e1c17576972c6f761b419b27f2
AmanWorku <amexworku2014@gmail.com>
2023-07-03 22:34:11 +0300

    Fix rubocop linter errors

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,8 +1,7 @@
-class Payment <ApplicationRecord
-    belongs_to :user
-    has_many :category, dependent: :destroy
+class Payment < ApplicationRecord
+  belongs_to :user
+  has_many :category, dependent: :destroy
 
-    validates :name, presence: true, length: { maximum: 25 }
-    validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
-
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :amount, presence: true, numericality: { greater_than: 0, only_integer: true }
 end
